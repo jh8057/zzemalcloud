4bee82a06613d8408bffb166f74e64aed99517df
jh8057 <jh8057@users.noreply.github.com>
2022-02-06 15:37:35 +0000

    deploy: a37d21dc57e94ed7f504160a6eba80deddc099fa

---
+++ _nuxt/static/1644161854/DbMain/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/DbMain", {data:[{}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1644161854/DbMain/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fzzemalcloud\u002F_nuxt\u002Fstatic\u002F1644161854",layout:"default",error:a,serverRendered:true,routePath:"\u002FDbMain",config:{_app:{basePath:"\u002Fzzemalcloud\u002F",assetsPath:"\u002Fzzemalcloud\u002F_nuxt\u002F",cdnURL:a}}}}(null));
