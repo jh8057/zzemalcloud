49013be8f07ca308d2d568475397e3dc8319fcdf
jh8057 <69341843+jh8057@users.noreply.github.com>
2022-02-17 23:55:31 +0900

    typescript

---
+++ components/typescriptComp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <input type="text" v-model="message" />
+    <div>{{ message }}</div>
+  </div>
+</template>
+
+<script>
+import Vue from "vue/types/umd";
+
+export default class Message extends Vue {
+  message: String = "메세지를 입력해주세요";
+}
+</script>
+
+<style>
+</style>
