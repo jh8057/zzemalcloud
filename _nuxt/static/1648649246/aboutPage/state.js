8bf7ad203464fe9fe7021409105e57f072cd5f8a
jh8057 <jh8057@users.noreply.github.com>
2022-03-30 14:07:26 +0000

    deploy: 7e629028751938ae6e81e99ff3e8066cc2238582

---
+++ _nuxt/static/1648649246/aboutPage/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fzzemalcloud\u002F_nuxt\u002Fstatic\u002F1648649246",layout:"default",error:a,serverRendered:true,routePath:"\u002FaboutPage",config:{_app:{basePath:"\u002Fzzemalcloud\u002F",assetsPath:"\u002Fzzemalcloud\u002F_nuxt\u002F",cdnURL:a}}}}(null));
