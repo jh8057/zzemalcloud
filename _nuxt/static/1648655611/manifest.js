b4ec6a889c2ba49f2a786eb73e0082c885744b49
jh8057 <jh8057@users.noreply.github.com>
2022-03-30 15:53:32 +0000

    deploy: ba9ad1642eeaaa94977874d2c43e18073e2c217f

---
+++ _nuxt/static/1648655611/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002FDbMain","\u002FaboutPage","\u002FDiaryMain","\u002FopenApi","\u002Ftypescript"]})
---
+++ _nuxt/static/1648655611/openApi/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/openApi", {data:[{}],fetch:{},mutations:void 0});
