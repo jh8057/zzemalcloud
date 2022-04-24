41272f30a21dc898630b3e6f9c8bf6f14ffd32bf
jh8057 <luckysymbol13@gmail.com>
2022-04-24 23:45:43 +0900

    jest - message beforeEach()

--- test/message.spec.js
+++ test/message.spec.js
@@ -2,21 +2,20 @@ import { mount } from "@vue/test-utils";
 import message from "@/components/message.vue";
 
 describe("message - component", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(message);
+  });
   test("is a Vue instance - message", () => {
-    const wrapper = mount(message);
     expect(wrapper.vm).toBeTruthy();
   });
   test("has a input tag", () => {
-    const wrapper = mount(message);
     expect(wrapper.find("input").isVisible()).toBeTruthy();
   });
   test("span message ", () => {
-    const wrapper = mount(message);
     expect(wrapper.find("span").isVisible()).toBeTruthy();
   });
   test("default message - this is mesg", () => {
-    const wrapper = mount(message);
-    expect(wrapper.find("span").text()).toBe('this is mesg');
+    expect(wrapper.find("span").text()).toBe("this is mesg");
   });
-
 });
