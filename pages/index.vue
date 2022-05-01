5485dbd80e2d583d6e4a04627b44e811ebc3a3c6
jh8057 <luckysymbol13@gmail.com>
2022-05-01 23:02:16 +0900

    Add menu
    on Main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="index__wrapper">
     <login-comp />
-    <nuxt-link to="/openApi"> API Page </nuxt-link> <br />
-    <nuxt-link to="/aboutPage"> About Page </nuxt-link> <br />
-    <nuxt-link to="/DiaryMain"> Diary </nuxt-link> <br />
-    <nuxt-link to="/DbMain"> DB </nuxt-link><br />
-    <nuxt-link to="/typescript"> typescript </nuxt-link><br />
-    <nuxt-link to="/randomPage"> randomPage </nuxt-link><br />
+    <article class="menu">
+      <h1>- Menu -</h1>
+      <nuxt-link to="/openApi"> API Page </nuxt-link> <br />
+      <nuxt-link to="/aboutPage"> About Page </nuxt-link> <br />
+      <nuxt-link to="/DiaryMain"> Diary </nuxt-link> <br />
+      <nuxt-link to="/DbMain"> DB </nuxt-link><br />
+      <nuxt-link to="/typescript"> typescript </nuxt-link><br />
+      <nuxt-link to="/randomPage"> randomPage </nuxt-link><br />
+    </article>
     <footer-one />
   </div>
 </template>
