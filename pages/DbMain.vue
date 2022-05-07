b4ef34463a1454f6afbcbfd676dbf152cebadb84
jh8057 <luckysymbol13@gmail.com>
2022-05-07 22:53:46 +0900

    merge aboutPage to DbMain

--- pages/DbMain.vue
+++ pages/DbMain.vue
@@ -1,33 +1,37 @@
 <template>
   <div class="dbPage__wrapper">
-      <h2>DB Test</h2>
-      <button @click="getDbData"> click</button>
-      <div>
-        {{name}} / {{contents}}
-      </div>
+    <h2>server Example</h2>
+    <button @click="getExampleData">Get Data!</button>
+    {{ serverExample }}
+    <hr />
+    <h2>DB Test</h2>
+    <button @click="getDbData">click</button>
+    <div>{{ name }} / {{ contents }}</div>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
-      name : 'no',
-      contents : 'no',
-    }
+      name: "no",
+      contents: "no",
+      serverExample: "",
+    };
   },
-  methods:{
-    async getDbData(){
+  methods: {
+    async getDbData() {
       let result = await this.$axios.$get("api/db/users");
-      console.log(result)
-      this.name = result[0].name
-      this.contents = result[0].contents
+      console.log(result);
+      this.name = result[0].name;
+      this.contents = result[0].contents;
     },
-  }
-
-}
+    async getExampleData() {
+      let result = await this.$axios.$get("/about");
+      this.serverExample = result.msg;
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,6 @@
     <article class="menu">
       <h1>- Menu -</h1>
       <nuxt-link to="/openApi"> API Page </nuxt-link> <br />
-      <nuxt-link to="/aboutPage"> About Page </nuxt-link> <br />
       <nuxt-link to="/DiaryMain"> Diary </nuxt-link> <br />
       <nuxt-link to="/DbMain"> DB </nuxt-link><br />
       <nuxt-link to="/typescript"> typescript </nuxt-link><br />
