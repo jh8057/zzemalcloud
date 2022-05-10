97dd0f4913822050627099bb0045a55af79ed945
jh8057 <luckysymbol13@gmail.com>
2022-05-10 23:20:47 +0900

    qrcode

---
+++ components/QrCode.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="qrCode">
+        <h2>QrCode</h2>
+        <p>Visit my portfolio site!</p>
+        <img src="https://qrtag.net/api/qr_4.png?url=https://jh8057.github.io/zzemal/projects/00.portfolio/" alt="qrtag" />
+        <p>▲ My Site</p>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- pages/typescript.vue
+++ pages/typescript.vue
@@ -1,41 +1,45 @@
 <template>
-  <div>
-    <h1>Typescript</h1>
-    <message></message>
-    <input v-model="var1" /> *
-    <input v-model="var2" />
-    <button @click="calculatorMulti">click</button>
-    <button @click="printHello">printHello</button>
-    <div>{{ answer }}</div>
-  </div>
+    <div>
+        <h1>Typescript</h1>
+        <message></message>
+        <input v-model="var1" /> *
+        <input v-model="var2" />
+        <button @click="calculatorMulti">click</button>
+        <button @click="printHello">printHello</button>
+        <div>{{ answer }}</div>
+
+        <qr-code />
+    </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import message from "~/components/message.vue";
+import { Component, Vue } from 'vue-property-decorator';
+import message from '~/components/message.vue';
+import QrCode from '~/components/QrCode.vue';
 
 @Component({
-  components: {
-    message,
-  },
+    components: {
+        message,
+        QrCode,
+    },
 })
 @Component
 export default class extends Vue {
-  answer: number = 0;
-  var1: number = 0;
-  var2: number = 0;
+    answer: number = 0;
+    var1: number = 0;
+    var2: number = 0;
 
-  printHello(): void {
-    console.log("HELLO");
-  }
-
-  calculatorMulti(a: number, b: number): number {
-    if (isNaN(this.var1) || isNaN(this.var2)) {
-      alert("input Number!");
-      return 0;
+    printHello(): void {
+        console.log('HELLO');
     }
-    this.answer = this.var1 * this.var2;
-    return a * b;
-  }
+
+    calculatorMulti(a: number, b: number): number {
+        if (isNaN(this.var1) || isNaN(this.var2)) {
+            alert('input Number!');
+            return 0;
+        }
+        this.answer = this.var1 * this.var2;
+        return a * b;
+    }
 }
 </script>
