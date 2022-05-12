47ffb008dd630c769e356377129fc5a5f8c456cc
jh8057 <luckysymbol13@gmail.com>
2022-05-12 22:53:18 +0900

    actions & getters

--- components/UseStore.vue
+++ components/UseStore.vue
@@ -3,41 +3,35 @@
         <h2>Use Store</h2>
         {{ counter }}
         <button @click="increment">+1</button>
+        <button @click="doIncrement">+1</button>
+        <button @click="decrement">-1</button>
+        {{ getArrN }}
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import { mapState, mapMutations } from 'vuex';
+import { mapState, mapMutations, mapActions, mapGetters } from 'vuex';
 // import Vuex, { StoreOptions } from 'vuex';
-// Vue.use(Vuex);
 
 // interface State {
 //     // 상태값에 대한 인터페이스
 //     counter: number;
 // }
 
-// const Store: StoreOptions<State> = {
-//     state: {
-//         // 상태값
-//     },
-//     mutations: {
-//         // 상태 변이 함수
-//     },
-//     actions: {
-//         // 상태 변이를 위한 로직
-//     },
-//     getters: {
-//         // 계산된 값일 반환
-//     },
-// };
-
 export default Vue.extend({
     computed: {
         ...mapState(['counter']),
+        ...mapGetters({
+            // map `this.$store.getters.doneTodosCount`
+            getArrN: 'getArrNumber',
+        }),
     },
     methods: {
-        ...mapMutations(['increment']),
+        ...mapMutations(['increment', 'decrement']),
+        ...mapActions({
+            doIncrement: 'increment',
+        }),
     },
     mounted() {
         console.log(this.counter);
--- store/index.js
+++ store/index.js
@@ -2,10 +2,26 @@
 
 export const state = () => ({
     counter: 0,
+    arr: [1, 2, 'string'],
 });
 
 export const mutations = {
     increment(state) {
         state.counter++;
+    },
+    decrement(state) {
+        state.counter--;
+    },
+};
+
+export const actions = {
+    increment(context) {
+        context.commit('increment');
+    },
+};
+
+export const getters = {
+    getArrNumber(state) {
+        return state.arr.filter((el) => typeof el === 'number');
     },
 };
