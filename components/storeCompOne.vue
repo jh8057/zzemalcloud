65937e9440860b2b05b5375520336691b1f23713
jh8057 <luckysymbol13@gmail.com>
2022-05-13 22:14:49 +0900

    store comp

---
+++ components/storeCompOne.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="storeCompOne"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ pages/storePage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="storePage"></div>
+</template>
+
+<script>
+import storeCompOne from '~/components/storeCompOne.vue';
+import storeCompTwo from '~/components/storeCompOne.vue';
+export default {
+    components: {
+        storeCompOne,
+        storeCompTwo,
+    },
+};
+</script>
+
+<style></style>
