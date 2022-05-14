9c743c796a1abab3e357d9935ef6799232fc5ae4
jh8057 <luckysymbol13@gmail.com>
2022-05-14 23:08:27 +0900

    store

--- pages/storePage.vue
+++ pages/storePage.vue
@@ -1,14 +1,20 @@
 <template>
-    <div class="storePage"></div>
+    <div class="storePage">
+        {{ click }}
+    </div>
 </template>
 
-<script>
+<script lang="ts">
 import storeCompOne from '~/components/storeCompOne.vue';
 import storeCompTwo from '~/components/storeCompOne.vue';
+import { mapState } from 'vuex';
 export default {
     components: {
         storeCompOne,
         storeCompTwo,
+    },
+    computed: {
+        ...mapState(['multi/click']),
     },
 };
 </script>
---
+++ store/multi.js
@@ -0,0 +1,7 @@
+export const state = () => ({
+    nowPage: 0,
+    pageSize: 0,
+    arr: [],
+    click: 0,
+    from: '',
+});
