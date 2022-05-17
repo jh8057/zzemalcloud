4244ec4326a9589564e73261d38ae94ba72441e7
jh8057 <luckysymbol13@gmail.com>
2022-05-17 22:50:19 +0900

    store - basic

--- components/storeCompOne.vue
+++ components/storeCompOne.vue
@@ -1,16 +1,30 @@
 <template>
     <div class="storeCompOne">
         <h2>One</h2>
-        {{ click }}
+        <div class="click">
+            {{ click }} <button @click="AddClick">+1</button>
+            <button @click="minusClick">-1</button>
+        </div>
+        <div class="pageInfo">page Info : {{ GET_pageInfo }} <button @click="addFrom('One-comp')">from</button></div>
     </div>
 </template>
 
 <script lang="ts">
-import { mapState } from 'vuex';
+import { mapState, mapMutations, mapActions, mapGetters } from 'vuex';
 export default {
     computed: {
         ...mapState('multi', {
             click: (state: any) => state.click,
+        }),
+        ...mapGetters('multi', {
+            GET_pageInfo: 'GET_pageInfo',
+        }),
+    },
+    methods: {
+        ...mapActions('multi', {
+            AddClick: 'AddClick',
+            minusClick: 'minusClick',
+            addFrom: 'addFrom',
         }),
     },
 };
--- store/multi.js
+++ store/multi.js
@@ -5,3 +5,33 @@ export const state = () => ({
     click: 0,
     from: '',
 });
+
+export const mutations = {
+    upClick(state) {
+        state.click++;
+    },
+    downClick(state) {
+        state.click--;
+    },
+    changeFrom(state, payload) {
+        state.from = payload;
+    },
+};
+
+export const actions = {
+    AddClick(context) {
+        context.commit('upClick');
+    },
+    minusClick(context) {
+        context.commit('downClick');
+    },
+    addFrom(context, payload) {
+        context.commit('changeFrom', payload);
+    },
+};
+
+export const getters = {
+    GET_pageInfo(state) {
+        return { nowPage: state.nowPage, pageSize: state.pageSize, from: state.from };
+    },
+};
