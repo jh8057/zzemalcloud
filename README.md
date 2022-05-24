64773943b55508fe591687c87c2d55b109b2937c
jh8057 <luckysymbol13@gmail.com>
2022-05-24 23:49:08 +0900

    sweetalert2

--- README.md
+++ README.md
@@ -45,6 +45,7 @@ Docker 와 GCP Cloud 를 이용한 서비스를 만들어 보기 위한 프로�
 -   vue-Typescript : Using extend [link](https://kyounghwan01.github.io/blog/TS/vue-use-typescript/#vue-js-%E1%84%8C%E1%85%A9%E1%86%BC%E1%84%92%E1%85%A1%E1%86%B8-%E1%84%8B%E1%85%A8%E1%84%89%E1%85%B5)
 -   vue-Typescript : [link](https://romeoh.tistory.com/entry/VUE-TYPESCRIPT-Vuex-Interface%EB%A5%BC-%EA%B5%AC%ED%98%84%ED%95%98%EB%8A%94-Store)
 -   vue-Typescript : [link](https://kdydesign.github.io/2019/04/06/vuejs-vuex-helper/)
+-   sweetalert2 : [link](https://sweetalert2.github.io/#examples)
 
 ## Docker
 
--- components/LoginComp.vue
+++ components/LoginComp.vue
@@ -1,85 +1,86 @@
 <template>
-  <div class="LoginWrapper">
-    <fieldset>
-      <legend>환영합니다🍰</legend>
-      I D : <input class="input" placeholder="아이디" v-model="memID" />
-      <br />
-      PW:
-      <input
-        class="input"
-        placeholder="비밀번호"
-        type="password"
-        v-model="memPW"
-      />
-      <!-- <button type="submit" @click="">Login</button> -->
-      <a @click="login">Login</a>
-    </fieldset>
-    <p>HINT : U BirthDay / J BirthDay</p>
-    <naver-login />
-    <button @click="tokenCheck">Login Check</button>
-  </div>
+    <div class="LoginWrapper">
+        <fieldset>
+            <legend>환영합니다🍰</legend>
+            I D : <input class="input" placeholder="아이디" v-model="memID" />
+            <br />
+            PW:
+            <input class="input" placeholder="비밀번호" type="password" v-model="memPW" />
+            <!-- <button type="submit" @click="">Login</button> -->
+            <a @click="login">Login</a>
+        </fieldset>
+        <p>HINT : U BirthDay / J BirthDay</p>
+        <naver-login />
+        <button @click="tokenCheck">Login Check</button>
+    </div>
 </template>
 
 <script>
-import naverLogin from "./naverLogin.vue";
+import naverLogin from './naverLogin.vue';
+import Swal from 'sweetalert2';
 
 export default {
-  components: {
-    naverLogin,
-  },
-  data() {
-    return {
-      token: "",
-      memID: "",
-      memPW: "",
-    };
-  },
-  methods: {
-    async login() {
-      const params = {
-        memID: this.memID,
-        memPW: this.memPW,
-      };
-      let result = await this.$axios.$post("api/user/login", params);
-      console.log(result);
-      if (result.token) {
-        this.token = result.token;
+    components: {
+        naverLogin,
+    },
+    data() {
+        return {
+            token: '',
+            memID: '',
+            memPW: '',
+        };
+    },
+    methods: {
+        async login() {
+            const params = {
+                memID: this.memID,
+                memPW: this.memPW,
+            };
+            let result = await this.$axios.$post('api/user/login', params);
+            console.log(result);
+            if (result.token) {
+                this.token = result.token;
 
-        //set Cookie
-        this.$router.push("/DiaryMain");
-      }
+                //set Cookie
+                this.$router.push('/DiaryMain');
+            }
+        },
+        async tokenCheck() {
+            const params = {
+                token: this.token,
+            };
+            if (this.token) {
+                let result = await this.$axios.$get('api/user/tokenCheck', { params });
+                console.log(result);
+            } else {
+                Swal.fire({
+                    title: 'Login해주세요',
+                    text: '관계자 전용 로그인 입니다.',
+                    icon: 'error',
+                    confirmButtonText: 'OK',
+                });
+            }
+        },
     },
-    async tokenCheck() {
-      const params = {
-        token: this.token,
-      };
-      if (this.token) {
-        let result = await this.$axios.$get("api/user/tokenCheck", { params });
-        console.log(result);
-      } else {
-        alert("Login해주세요");
-      }
-    },
-  },
 };
 </script>
 
 <style>
 legend {
-  margin: 0;
-  width: 40vh;
-  justify-content: center;
-  text-align: center;
-  background-color: rgb(29, 65, 0);
-  color: #fff;
-  padding: 3px 6px;
+    margin: 0;
+    width: 40vh;
+    justify-content: center;
+    text-align: center;
+    background-color: rgb(29, 65, 0);
+    color: #fff;
+    padding: 3px 6px;
 }
 fieldset > a {
-  margin-left: 5px;
-  position: relative;
-  top: -10px;
-  background-color: rgb(37, 59, 46);
-  padding: 10px;
-  color: white;
+    margin-left: 5px;
+    position: relative;
+    top: -10px;
+    background-color: rgb(37, 59, 46);
+    padding: 10px;
+    color: white;
 }
 </style>
